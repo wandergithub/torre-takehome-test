6c46ac7cca7d08ded0ad3e051a76b9b295af2b96
wandergithub <wandergonzalez0@hotmail.com>
2023-04-06 11:07:56 -0400

    Change fetch user request to work with rails API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import "./App.css";
-import { useState } from "react";
-import axios from "axios";
-import Header from "./components/Header";
-import { proficiencies, svgs, exampleState } from "./data";
-import SkillDetails from "./components/SkillDetails";
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Header from './components/Header';
+import { proficiencies, svgs, exampleState } from './data';
+import SkillDetails from './components/SkillDetails';
 
 function App() {
   // Current username serch
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState('');
   // Data fetched of the username
-  const [data, setData] = useState(exampleState);
+  const [data, setData] = useState();
   // Current Skill object for Details pop-up window
   const [skill, setSkill] = useState(null);
 
   const fetchUserData = async () => {
-    // const response = await axios.get(`https://torre.bio/api/bios/${userName}`);
-    // setData(response.data);
+    const response = await axios.get(`http://127.0.0.1:3000/users/${userName}`);
+    setData(response.data);
   };
 
   return (
@@ -23,8 +23,8 @@ function App() {
       <Header setUserName={setUserName} fetchUserData={fetchUserData} />
       {data && (
         <div className="px-3">
-          <img src={data.person.picture} alt="Profile" />
-          <h3 className="text-center mt-5 heading-1">{data.person.name}</h3>
+          <img src={data.picture} alt="Profile" />
+          <h3 className="text-center mt-5 heading-1">{data.name}</h3>
           <div className="my-5">
             <h4 className="heading-2 my-4">Skills and interests:</h4>
             {proficiencies.map((categorie, index) => (
@@ -32,9 +32,9 @@ function App() {
                 <div className="d-flex gap-2 icon">
                   {svgs[index]}
                   <h5 className="heading-3 my-3 text-capitalize">
-                    {categorie !== "no-experience-interested"
+                    {categorie !== 'no-experience-interested'
                       ? categorie
-                      : "No experience but interested"}
+                      : 'No experience but interested'}
                   </h5>
                 </div>
                 <ul>
@@ -58,7 +58,7 @@ function App() {
           </div>
         </div>
       )}
-      {skill && <SkillDetails setSkill={setSkill} skill={skill} name={data.person.name} />}
+      {skill && <SkillDetails setSkill={setSkill} skill={skill} name={data.name} />}
     </div>
   );
 }
